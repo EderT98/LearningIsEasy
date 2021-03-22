604dae95f7a205ca17b6e1fcd430d4d43e12c8fb
EderT98 <edertassin@gmail.com>
2021-03-21 21:37:47 -0300

    Creacion de proyecto

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,17 @@
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+	<div>
+		<div>
+			<h3>Video Introduccion python</h3>
+			<a href="https://www.youtube.com/channel/UCMqY16CUHOQvKepPW-f9M8Q">Leonardo Kuffo</a>
+			<br>
+			<span>
+				<iframe width="560" height="315" src="https://www.youtube.com/embed/G5eO8PfWfkw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			</span>
+		</div>
+	</div>
 </main>
 
 <style>
